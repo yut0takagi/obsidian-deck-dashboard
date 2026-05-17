1977c456db249cd9bc0a620cf8b4161caad93ba0
Yuto Taakgi <yutotkg.1040@gmail.com>
2026-05-17 15:48:47 +0900

    feat: wire plugin entrypoint, register view + extension

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,24 @@
-export default {};
+import { Plugin } from "obsidian";
+import { DashboardView } from "./core/DashboardView";
+import {
+  DASHBOARD_EXTENSION,
+  VIEW_TYPE_DASHBOARD,
+} from "./core/constants";
+import { registerCommands } from "./commands";
+
+export default class NotionDashboardPlugin extends Plugin {
+  async onload(): Promise<void> {
+    this.registerView(VIEW_TYPE_DASHBOARD, (leaf) => new DashboardView(leaf));
+    this.registerExtensions([DASHBOARD_EXTENSION], VIEW_TYPE_DASHBOARD);
+
+    registerCommands(this);
+
+    this.addRibbonIcon("layout-dashboard", "Open home dashboard", () => {
+      (this.app as any).commands.executeCommandById("notion-dashboard:open-home");
+    });
+  }
+
+  async onunload(): Promise<void> {
+    // registerView and registerExtensions are auto-cleaned by Plugin lifecycle.
+  }
+}
